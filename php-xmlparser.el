362972f2f10fc33ab8f2016feaaf7263c0b87877
David Arroyo Menéndez <davidam@gnu.org>
2015-05-15 12:34:56 +0200

    php-dio.el, php-xmlparser.el: add copyright note

--- php-xmlparser.el
+++ php-xmlparser.el
@@ -1,6 +1,26 @@
-;; XML Parser
-;; More see /usr/share/doc/php-doc/html/function.xml-parse-into-struct.html
+;; Copyright (C) 2015  David Arroyo Menéndez
 
+;; Author: David Arroyo Menéndez <davidam@gnu.org>
+;; Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+;; Boston, MA 02110-1301 USA,
+
+;; XML Parser
+;; More see file:///usr/share/doc/php-doc/html/function.xml-parse-into-struct.html
+;; http://php.net/manual/en/book.xml.php
 
 (define-skeleton php-utf8_decode
   "Insert a utf8_decode statement"
