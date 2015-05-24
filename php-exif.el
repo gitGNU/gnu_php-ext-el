98b1434855823ab52a8fa131f029a25306af9c86
David Arroyo7 <davidam@gnu.org>
2015-05-24 16:36:32 +0200

    php-exif.el: creation

---
+++ php-exif.el
@@ -0,0 +1,29 @@
+;; Copyright (C) 2015  David Arroyo Menéndez
+
+;; Author: David Arroyo Menéndez <davidam@gnu.org>
+;; Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+;; Boston, MA 02110-1301 USA,
+
+;; Exif functions
+;; http://php.net/manual/en/ref.exif.php
+;; file:///usr/share/doc/php-doc/html/ref.exif.html
+
+
+(define-skeleton php-exif_imagetype 
+  "Insert an exif_imagetype statement. Determine the type of an image"
+  > "exif_imagetype(" (skeleton-read "Filename? ") ");"
+)
