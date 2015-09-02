d764571f6282b4b51e52ef3eac1e4d52a8feff90
David Arroyo7 <davidam@gnu.org>
2015-09-02 23:52:37 +0200

    php-array.el: add 19 functions

--- php-array.el
+++ php-array.el
@@ -116,3 +116,183 @@
     ", " str)
   > ");" \n
 )
+
+(define-skeleton php-array_fill_keys
+  "Insert a array_fill_keys statement."
+  ""
+  '(setq keys (skeleton-read "keys: "))
+  '(setq value (skeleton-read "value: "))
+  > "array_fill_keys(" keys ", " value ");" \n
+)
+
+(define-skeleton php-array_fill
+  "Insert a array_fill statement."
+  ""
+  '(setq start_index (skeleton-read "start_index: "))
+  '(setq num (skeleton-read "num: "))
+  '(setq value (skeleton-read "value: "))
+  > "array_fill(" start_index ", " num ", " value ");" \n
+)
+
+(define-skeleton php-array_filter
+  "Insert a array_filter statement."
+  ""
+  '(setq array (skeleton-read "array: "))
+  '(setq callback (skeleton-read "callback: "))
+  > "array_filter(" array ", " callback ");" \n
+)
+
+(define-skeleton php-array_flip
+  "Insert a array_flip statement."
+  ""
+  '(setq array (skeleton-read "array: "))
+  > "array_flip(" array ");" \n
+)
+
+(define-skeleton php-array_intersect_key
+  "Insert a array_intersect_key statement. Computes the intersection of arrays using keys for comparison"
+  ""
+  '(setq array1 (skeleton-read "array1: "))
+  '(setq array2 (skeleton-read "array2: "))
+  > "array_intersect_key(" array1 ", " array2 
+  ( "another array?, %s: "
+    ", " str)
+  ");" \n
+)
+
+(define-skeleton php-array_intersect_uassoc
+  "Insert a array_intersect_uassoc statement. "
+  ""
+  '(setq array1 (skeleton-read "array1: "))
+  '(setq array2 (skeleton-read "array2: "))
+  > "array_intersect_uassoc(" array2 ", " array2 
+  ( "another array?, %s: "
+    ", " str)
+  > ", " (skeleton-read "key compare function: ")
+  ");" \n
+)
+
+(define-skeleton php-array_intersect_ukey
+  "Insert a array_intersect_ukey statement. Computes the intersection of arrays using a callback function on the keys for comparison"
+  ""  
+  '(setq array1 (skeleton-read "array1: "))
+  '(setq array2 (skeleton-read "array2: "))
+  > "array_intersect_ukey(" array1 ", " array2 
+  ( "another array?, %s: "
+    ", " str)
+  > ", " (skeleton-read "key compare function: ")
+  ");" \n
+)
+
+(define-skeleton php-array_intersect
+  "Insert a array_intersect statement. Computes the intersection of arrays
+"
+  ""
+  '(setq array1 (skeleton-read "array1: "))
+  '(setq array2 (skeleton-read "array2: "))
+  > "array_intersect(" array1 ", " array2 
+  ( "another array?, %s: "
+    ", " str)
+  ");" \n
+)
+
+(define-skeleton php-array_key_exists
+  "Insert a array_key_exists statement. Checks if the given key or index exists in the array"
+  ""
+  '(setq key (skeleton-read "key: "))
+  '(setq array (skeleton-read "array: "))
+  > "array_key_exists(" key ", " array ");" \n
+)
+
+(define-skeleton php-array_keys
+  "Insert a array_keys statement. Return all the keys or a subset of the keys of an array"
+  ""
+  '(setq array (skeleton-read "array: "))
+  '(setq search_value (skeleton-read "search_value: "))
+  '(setq strict (skeleton-read "strict: "))
+  > "array_keys(" array ", " search_value ", " strict ");" \n
+)
+
+(define-skeleton php-array_map
+  "Insert a array_map statement. Applies the callback to the elements of the given arrays"
+  ""
+  '(setq callback (skeleton-read "callback: "))
+  '(setq array1 (skeleton-read "array1: "))
+  > "array_map(" callback ", " array1
+  ( "another array?, %s: "
+    ", " str)
+  ");" \n
+)
+
+(define-skeleton php-array_merge_recursive
+  "Insert a array_merge_recursive statement. Merge two or more arrays recursively"
+  ""
+  '(setq array1 (skeleton-read "array1: "))
+  > "array_merge_recursive(" array1 
+  ( "another array?, %s: "
+    ", " str)
+  ");" \n
+)
+
+(define-skeleton php-array_merge
+  "Insert a array_merge statement. Merge two or more arrays recursively"
+  ""
+  '(setq array1 (skeleton-read "array1: "))
+  > "array_merge(" array1 
+  ( "another array?, %s: "
+    ", " str)
+  ");" \n
+)
+
+(define-skeleton php-array_multisort
+  "Insert a array_multisort statement. Sort multiple or multi-dimensional arrays"
+  ""
+  '(setq array1 (skeleton-read "array1: "))
+  '(setq array1_sort_order (skeleton-read "array1_sort_order: "))
+  '(setq array1_sort_flags (skeleton-read "array1_sort_flags: "))
+  > "array_multisort(" array1 ", " array1_sort_order ", " array_sort_flags ");" \n
+)
+
+(define-skeleton php-array_pad
+  "Insert a array_pad statement. Pad array to the specified length with a value"
+  ""
+  '(setq array (skeleton-read "array: "))
+  '(setq size (skeleton-read "size: "))
+  '(setq value (skeleton-read "value: "))
+  > "array_pad(" array ", " size ", " value ");" \n
+)
+
+(define-skeleton php-array_pop
+  "Insert a array_pop statement."
+  ""
+  '(setq array (skeleton-read "array: "))
+  > "array_pop(" array ");" \n
+)
+
+(define-skeleton php-array_product
+  "Insert a array_product statement."
+  ""
+  '(setq array (skeleton-read "array: "))
+  > "array_product(" array ");" \n
+)
+
+(define-skeleton php-array_push
+  "Insert a array_push statement."
+  ""
+  '(setq array (skeleton-read "array: "))
+  '(setq value1 (skeleton-read "value1: "))
+  > "array_push(" array ", " value1
+  ( "another variable?, %s: "
+    ", " str)
+  ");" \n
+)
+
+(define-skeleton php-array_rand
+  "Insert a array_rand statement."
+  ""
+  '(setq array (skeleton-read "array: "))
+  '(setq num (skeleton-read "num: "))
+  > "array_rand(" array ", " num ");" \n
+)
+
+
